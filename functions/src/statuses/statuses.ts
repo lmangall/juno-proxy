1b48dd8d996a56d6a68928eb8ed85029f8216dda
David Dal Busco <david.dalbusco@outlook.com>
2023-04-10 19:08:09 +0200

    feat: api change to collect with a delta time not date

--- functions/src/statuses/statuses.ts
+++ functions/src/statuses/statuses.ts
@@ -3,12 +3,11 @@ import {filterStatuses} from "../utils/status.utils";
 
 export const collectStatuses = async () => {
   try {
-    const now = BigInt(Date.now() * 1e6);
     const oneMin = 60_000_000_000n;
 
     const actor = await observatoryActor();
     const statuses = await actor.collect_statuses({
-      collected_after: [now - oneMin],
+      time_delta: [oneMin],
     });
 
     console.log("Statuses collected: ", statuses.filter(filterStatuses));
