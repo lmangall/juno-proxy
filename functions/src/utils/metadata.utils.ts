6456d4e0acbf8c35093a1cb2c55c22400cbf4fba
David Dal Busco <david.dalbusco@outlook.com>
2023-04-12 19:54:01 +0200

    chore: format

--- functions/src/utils/metadata.utils.ts
+++ functions/src/utils/metadata.utils.ts
@@ -2,7 +2,7 @@ export const metadataEmail = (metadata: [string, string][]): string =>
   metadataKey({metadata, key: "email"});
 
 export const metadataName = (metadata: [string, string][]): string =>
-    metadataKey({metadata, key: "name"});
+  metadataKey({metadata, key: "name"});
 
 const metadataKey = ({
   metadata,
