e30ed001b2eb0815afc579d9aad5f9226206a328
David Dal Busco <david.dalbusco@outlook.com>
2023-09-01 17:29:36 +0200

    build: ignore indent

--- functions/.eslintrc.cjs
+++ functions/.eslintrc.cjs
@@ -24,7 +24,7 @@ module.exports = {
   rules: {
     quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    indent: ["error", 2],
+    indent: "off",
     "quote-props": ["error", "as-needed"],
     camelcase: "off",
     "operator-linebreak": "off",
