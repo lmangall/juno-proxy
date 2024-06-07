7d472dd611f48f363addc1962d8e27e53b7c9ec5
David Dal Busco <david.dalbusco@outlook.com>
2024-06-07 13:00:56 +0200

    feat: declarations

--- functions/scripts/declarations.mjs
+++ functions/scripts/declarations.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import {cp} from "node:fs/promises";
 
-const src = "/Users/daviddalbusco/projects/juno/juno/src/declarations";
-const dest = ["./declarations/"];
+const src = "/Users/daviddalbusco/projects/juno/juno/src/declarations/observatory";
+const dest = ["./declarations/observatory"];
 
 const promises = dest.map((d) => cp(src, d, {recursive: true}));
 await Promise.all(promises);
