a1384656db68199c21c9a6b1abb0ff4488e4ab97
David Dal Busco <david.dalbusco@outlook.com>
2024-12-31 14:22:25 +0100

    build: update fibase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
-import * as functions from "firebase-functions";
+import * as functionsV1 from "firebase-functions/v1";
 import {collectStatuses} from "./statuses/statuses.js";
 
-export const statuses = functions
+export const statuses = functionsV1
   .runWith({secrets: ["CRON_CONTROLLER", "MAIL_PWD"]})
   .pubsub.schedule("every 15 minutes")
   .onRun(collectStatuses);
