093b66ec99ab65acdfc6bfacc291bce6362c5116
lmangall <l.mangallon@gmail.com>
2025-08-20 07:51:03 +0200

    config: update Firebase configuration files

--- firebase.env.sh
+++ firebase.env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-firebase functions:config:set notifications.token="secret"
+firebase functions:config:set notifications.token="jf93h287fh4983hjf928h37fgh483hf83h"
 
-firebase functions:config:set mail.resend.api_key="secret"
+firebase functions:config:set mail.resend.api_key="628ec930c76eae8664218d19dc881c12"
